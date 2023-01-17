8f457fd2c9974762e5736fbdcff443729046edb2
max_air <max_air@bk.ru>
2023-01-17 16:18:34 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsObj = Object.fromEntries(bracketsConfig);
+
+    let bracketsArr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsArr.length !== 0) {
+            if (bracketsObj[bracketsArr[bracketsArr.length - 1]] === str[i]) {
+                bracketsArr.pop();
+            } else {
+                bracketsArr.push(str[i]);
+            }
+        } else {
+            bracketsArr.push(str[i]);
+
+        }
+
+    }
+
+
+    return bracketsArr.length == 0;
 }
